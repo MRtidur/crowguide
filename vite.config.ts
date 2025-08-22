d156ea794d2ccef6651ebf9f2d54dde78c360e75
Zulfikar hadi <zulfikarhadi31@gmail.com>
2025-08-22 15:32:52 +0700

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: "/crowguide/",
+    plugins: [react(), tailwindcss()],
+    base: "/crowguide/",
+    build: {
+    outDir: "dist"
+  }
 })
